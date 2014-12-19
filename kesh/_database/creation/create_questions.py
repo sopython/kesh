e645b40624e742f6e1bf55a992bc1f22c7e85d89
Keiron Pizzey <kjpizzey@gmail.com>
2014-12-19 16:41:49 +0000

    Modified create_questions.py

--- kesh/_database/creation/create_questions.py
+++ kesh/_database/creation/create_questions.py
@@ -51,25 +51,25 @@ for event, elem in context:
              parse(v) if k in str_to_date else
              convert_tags(v) if k in str_to_list else
              v for k, v in elem.items()}
-        if d[convert('PostTypeId')] == 1 and set(d[convert('Tags')]).intersection(python_tags):
+        if d['post_type_id'] == 1 and set(d['tags']).intersection(python_tags):
             coll.insert(d)
             # Add the post id to a set for pickling. This will be used
             # in other creation files to only create answers/comments/etc
             # that belong to Python questions.
-            question_ids.add(d['Id'])
+            question_ids.add(d['id'])
             elem.clear()
             while elem.getprevious() is not None:
                 del elem.getparent()[0]
             i += 1
             if i % 1000 == 0:
-                s_option = (strftime('%H:%M:%S', gmtime()), d['Id'])
+                s_option = (strftime('%H:%M:%S', gmtime()), d['id'])
                 s = '{:s} : Id - {:d}\n'.format(*s_option)
                 print(s, end='')
                 f.write(s)
 
 print('Finished importing, now creating indices.')
 
-coll.ensure_index(convert('Id'))
+coll.ensure_index(convert('id'))
 
 f.close()
 
